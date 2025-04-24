92f1e70b1f64a33b44c6c2e6ab59ff3d191517a5
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-04-25 02:20:48 +0530

    Added all the CRUD operations so basically added the Create api, read api which fetchs the data present in the database , update api which updates an specefix record in the database and delete api which deletes the sepecefic record in the db through id which is unique

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
+const userModel = require('./model/user')
+require('dotenv');
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
@@ -9,7 +10,36 @@ app.get('/', (req, res)=>{
     res.send("Hello the server is ready!")
 })
 
-const PORT = process.env.PORT
+app.post('/libbook', (req, res)=>{
+    userModel.create(req.body)
+    .then((data)=> res.json(data))
+    .catch((err)=> res.json(err))
+})
+
+app.patch('/libbook/:id', (req, res)=>{
+    userModel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+    .then((data)=> res.json(data))
+    .catch((err)=> res.json(err))
+})
+
+app.patch('/libbook/:id', (req, res)=>{
+    userModel.findByIdAndDelete(req.params.id, {$set: req.body}, {new: true})
+    .then((data)=> res.json(data))
+    .catch((err)=> res.json(err))
+})
+
+mongoose.connect('mongodb://localhost:27017/myLibrary')
+.then(()=> console.log("Connected to the database!"))
+.catch((err)=> console.log("Error connecting to the database!"))
+
+app.get('/libdata', (req, res)=>{
+    userModel.find()
+    .then((data)=> res.json(data))
+    .catch((err)=> res.json(err))
+})
+
+
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, (req, res)=>{
     console.log(`Server is running at http://localhost:${PORT}`)
